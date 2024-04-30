61c4eb06f3b88958ca6e34832791af01b9e73536
Pedro Augusto <pedromartins036@gmail.com>
2024-04-30 00:25:39 -0300

    fix: script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-function calculadora(valor, qtd){}
+
+function login(){
+
+    var login = document.getElementById('Login').value;
+    var senha = document.getElementById('Senha').value;
+
+    if(login == "1234" && senha == "1234"){
+        alert('Sucesso');
+    }else{
+        alert("Usuario ou senha incorretos");
+    }
+
+}
+
