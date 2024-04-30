84bfd05ff1bf9b81d5fb1554b5c893e81f7446b7
Pedro Augusto <pedromartins036@gmail.com>
2024-04-30 00:25:49 -0300

    update: vinho.js

--- js/vinho.js
+++ js/vinho.js
@@ -1,7 +1,3 @@
-// Dados dos vinhos
-const vinhos = await (await fetch("../data/vinhos.json")).json()
- 
-
 // Declarando os elementos de input
 const nome_element = document.getElementById("nome")
 const cupom_element = document.getElementById("cupom-id")
@@ -9,25 +5,31 @@ const vinho_img_element = document.getElementById("vinho-img")
 const nome_uva_element = document.getElementById("nome-uva")
 const subtitulo_element = document.getElementById("subtitulo")
 const preco_element = document.getElementById("preco")
+const total_element = document.getElementById("total")
 const descricao_element = document.getElementById("descricao")
 const origem_nome_element = document.getElementById("nome-origem")
 const origem_img_element = document.getElementById("img-pais")
 const qtd_vinho_element = document.getElementById("qtd-vinho")
 
+var vinhos
+var vinho
 var tem_desconto = false
-var desconto = 0.0
+var desconto = 1
 var idVinho
+var total
 
 async function carregarVinho(){
     
+
+    vinhos = await (await fetch("../data/vinhos.json")).json()
     // Obtem o ID do vinho
     idVinho = new URLSearchParams(document.location.search).get("nome")
-    
     // Procura o vinho no JSON
-    const vinho = vinhos[idVinho]
+    vinho = vinhos[idVinho]
 
     // Altera os elementos para os valores dos dados obtidos do JSON
     nome_element.innerText = vinho['nome']
+    total_element.innerText = vinho['preco']
     vinho_img_element.src = `../assets/${vinho['imagem_vinho']}`
     nome_uva_element.innerText = vinho['uva']
     subtitulo_element.innerText = vinho['subtitulo']
@@ -41,8 +43,6 @@ async function carregarVinho(){
 function verificarEstoque(){
     const valorDigitado = qtd_vinho_element.value
 
-    const vinho = vinhos[idVinho]
-
     if(valorDigitado > vinho['estoque']){
         qtd_vinho_element.value = vinho['estoque']
     }
@@ -54,21 +54,39 @@ function aplicarDesconto(){
     if(cupom == "webdev"){
         desconto = 0.10
         tem_desconto = true
+
+        alert(`Parabéns! Você recebeu ${desconto * 100}% de desconto.`)
     }else{
-        desconto = 0.0
+        desconto = 1
         tem_desconto = false
     }
+    
+    verificarTotal()
+}
 
+function verificarTotal(){
+    const qtdVinhos = qtd_vinho_element.value
+    total = calculadora(vinho['preco'], qtdVinhos, tem_desconto, desconto)
+
+    total_element.innerText = total
 }
 
 
 function adicionaCarrinho(){
 
     const qtdVinhos = qtd_vinho_element.value
+
+    const alerta = qtdVinhos > 1 ? `Você adicionou ${qtdVinhos} vinhos no seu carrinho, com um valor total de R$${total}.` : `Você adicionou um vinho no seu carrinho, com um valor total de R$${total}.`
+    alert(alerta)
     
+}
 
-
-    calculadora()
+function calculadora(garrafa_valor, garrafa_quantidade, tem_desconto= false, desconto= 1){
     
-
+    if (tem_desconto){
+        return (garrafa_valor * garrafa_quantidade - ((garrafa_valor * garrafa_quantidade) * desconto)).toFixed(2)
+    }
+    else{
+        return (garrafa_valor * garrafa_quantidade).toFixed(2);
+    }     
 }
