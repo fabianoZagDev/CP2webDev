49db62e1dddf9efe008d61b5898eb85e9ad854b5
Fabiano Zague <161663128+fabianoZagDev@users.noreply.github.com>
2024-04-30 20:51:49 -0300

    finalizado catalogo

--- js/catalogo.js
+++ js/catalogo.js
@@ -13,6 +13,28 @@ async function carregarCatalogo(){
 function criarItem (key){
     const catalogoDiv = document.getElementById("catalogo")
     
-    let novoItem = document.createElement("div")
+    let novoItem = document.createElement("a")
+    novoItem.setAttribute("href", `vinho.html?nome=${key}`)
+    let divBox = document.createElement("div")
+    divBox.setAttribute("class", "boxVinho")
+    novoItem.appendChild(divBox)
+
+
+    divBox.innerHTML = 
+    `<img src="../assets/${vinhos[key]["imagem_vinho"]}" alt="imagem do vinho">
+    <div class="infosVinho">
+        <div class="container-textos">
+            <p class="nomeVinho">${vinhos[key]["nome"]}</p>
+            <p class="precoVinho">R$: ${vinhos[key]["preco"]}</p>         
+      
+        </div>      
+        <div class="origem">
+            <img src="../assets/${vinhos[key]["bandeira_pais_origem"]}" alt="bandeira Origem" class="bandeiraVinho">
+            <p>
+                ${vinhos[key]["pais_origem"]}
+            </p>
+        </div>
+    </div>`
     
-}
+    catalogoDiv.appendChild(novoItem)
+}    
